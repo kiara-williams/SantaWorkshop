d52d2860d008f416724e69a2db8c96ca0aa7a806
Kiara Williams <kiara.williams@outlook.com.au>
2020-04-15 15:06:11 +1000

    Add read file function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from array_queue import ArrayQueue
+from operator import itemgetter
 
 
 def assign(queue, days, assignments, cost, choice=0):
@@ -6,7 +7,7 @@ def assign(queue, days, assignments, cost, choice=0):
     if instance[-1] + days(instance[choice]) > 300:
         return assign(queue, days, assignments, cost, choice + 1)
     else:
-        assignments.append(instance[0], instance[choice])
+        assignments.append(set(instance[0], instance[choice]))
         queue.dequeue()
         if choice >= 0:
             cost += calculate_costs(choice, instance[-1])
@@ -20,10 +21,24 @@ def calculate_costs(c, p):
     # continue from here
 
 
+def read_file(f, q):
+    temp_list = []
+    in_file = open(f, 'r')
+    next(in_file)
+    for line in in_file:
+        line.strip()
+        new_line = list(line.split(','))
+        temp_list.append(new_line)
+    sorted(temp_list, key=itemgetter(-1))
+    for item in temp_list:
+        q.enqueue(item)
+
+
 def main():
     queue = ArrayQueue()
     cost = 0
-    assignments = list()
+    assignments = []
     days = {k: 0 for k in range(1, 101)}
+    read_file("family_data.csv", queue)
     while len(queue) > 0:
         assign(queue, days, assignments, cost)
